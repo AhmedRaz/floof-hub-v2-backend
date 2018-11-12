2ea06175d17a340b7b116268d75ac69543da6087
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-11-11 22:58:46 -0500

    modified migration files

--- db/migrate/20181112034411_create_locations.rb
+++ db/migrate/20181112034411_create_locations.rb
@@ -1,7 +1,11 @@
 class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
-
+      t.float :latitude
+      t.float :longitude
+      t.string :address
+      t.string :city
+      t.string :state
       t.timestamps
     end
   end
