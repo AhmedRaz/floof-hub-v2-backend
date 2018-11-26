4925ca751f97e7b9b5fa63a2e97588476028cad8
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-11-25 23:39:07 -0500

    modified user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email_address, :telephone, :company_id, :password, :profile_pic_src, :user_level)
+    end
 end
