dbfae773b2c94e6279fc668e40aa7d84020df9d8
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-12-12 23:55:45 -0500

    Modified User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
 
   has_many :pets
 
+  validates  :first_name, {presence: true, length: {minimum: 2, maximum: 24}}
+  validates  :last_name, {presence: true, length: {minimum: 2, maximum: 24}}
+  validates  :password, {length: {minimum: 6}}
+  validates  :email_address, {presence: true, length: {minimum: 6, maximum: 64}, uniqueness: true}
 end
